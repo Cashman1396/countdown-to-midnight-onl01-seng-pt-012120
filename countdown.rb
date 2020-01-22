2e57f62bdd14cddcc09bc86ce325b519f4196d8a
Chris Ashman <chrisashman96@gmail.com>
2020-01-22 22:06:20 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -9,11 +9,3 @@ end
 end
 
 
-def countdown_with_sleep(seconds_to_midnight)
-  while seconds_to_midnight > 0
-    puts "#{seconds_to_midnight} SECOND(S)!"
-    sleep(1)
-    seconds_to_midnight -= 1
-  end
-  "HAPPY NEW YEAR!"
-end
